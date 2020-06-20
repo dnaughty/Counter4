5d629d2e17f2a74352a8f77fca6f5d291c6ff4a7
Denis Naughton <mjolinor75@hotmail.com>
2020-06-20 17:19:47 -0400

    first commit

---
+++ Counter4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter4
+//
+//  Created by Admin on 6/20/20.
+//  Copyright © 2020 DASHtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
